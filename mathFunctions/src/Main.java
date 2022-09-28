ce0569586d182982ea7c0f87e0fd3e7301404946
olvericc <olv.ericlopes@gmail.com>
2022-09-27 22:38:58 -0300

    Math functions

---
+++ mathFunctions/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double x = 3.0;
+        double y = 4.0;
+        double z = -5.0;
+        double a, b, c;
+
+        // square root function
+        a = Math.sqrt(x);
+        System.out.println("Square root of " + x + " equals to: " + a);
+
+        // power function - potential
+        b = Math.pow(x, y);
+        System.out.println(x + " elevated to " + y + " equals to: " + b);
+
+        // absolute value function
+        c = Math.abs(z);
+        System.out.println("Absolute value of " + z + " equals to: " + c);
+
+    }
+}
