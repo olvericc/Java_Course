bf83be12a655777c362c1ef9342a8e335c874cf3
olvericc <olv.ericlopes@gmail.com>
2022-10-17 11:53:19 -0300

    sixth repetitive structure table test

---
+++ repetitiveStructureTableTest6/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 4;
+        int y = 0;
+        int i = 0;
+
+        while ( i < x ) {
+            i += 1;
+            y += i;
+            System.out.print(i);
+            System.out.println(y);
+
+        }
+    }
+}
