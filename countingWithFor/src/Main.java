1e023be316c657822ad1207f44368dd884097d49
olvericc <olv.ericlopes@gmail.com>
2022-10-17 20:22:13 -0300

    counting with repetitive structure (for) - progressive and regressive count

---
+++ countingWithFor/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // progressive count
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Value of i: " + i);
+        }
+        System.out.println();
+
+        // regressive count
+        for (int j = 4; j >= 0; j--) {
+            System.out.println("Value of j: " + j);
+        }
+    }
+}
