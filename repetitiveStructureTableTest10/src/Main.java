272f325257d88db4b95a4d1280359026f4a2b12d
olvericc <olv.ericlopes@gmail.com>
2022-11-01 16:21:13 -0300

    tenth repetitive structure table test

---
+++ repetitiveStructureTableTest10/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int y = 10;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print(i);
+            y = y + i;
+            System.out.println(y);
+        }
+    }
+}
