59e78bb41c286d897ce2ef7a928b16ea027b2042
olvericc <olv.ericlopes@gmail.com>
2022-11-06 10:53:36 -0300

    twelfth repetitive structure table test

---
+++ repetitiveStructureTableTest12/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 4;
+        int y = 0;
+        for (int i = 0; i<x; i++) {
+            y = y + i;
+        }
+        System.out.println(y);
+    }
+}
