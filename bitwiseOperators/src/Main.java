1a0e6dc2433c7cb004f0c5384ae67e421388567d
olvericc <olv.ericlopes@gmail.com>
2022-11-06 20:38:13 -0300

    bitwise operators in java (and-or-xor)

---
+++ bitwiseOperators/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // variables declaration
+        int number1 = 89;
+        int number2 = 60;
+
+        // learning about bitwise operators
+
+        // operator and
+        System.out.println(number1 & number2);
+
+        // operator or
+        System.out.println(number1 | number2);
+
+        //operator xor
+        System.out.println(number1 ^ number2);
+
+    }
+}
